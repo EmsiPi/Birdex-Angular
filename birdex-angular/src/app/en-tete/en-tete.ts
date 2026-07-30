9f7faee111d9cec3810471fd403e41089fc9b91f
EmsiPi <mariecharlotte2116@gmail.com>
2026-07-30 18:39:53 +0200

    Design des boutons

--- birdex-angular/src/app/en-tete/en-tete.ts
+++ birdex-angular/src/app/en-tete/en-tete.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,5 +8,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './en-tete.css',
 })
 export class EnTete {
+  @Input() backgroundColorCarnet = 'rgb(241, 239, 239)';
+  @Input() backgroundColorPok = 'rgb(241, 239, 239)';
 
 }
